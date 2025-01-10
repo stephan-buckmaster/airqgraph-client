a146e8c02a7c956dcb2ec4572e7e8380004ae82f
Stephan Wehner <stephan@buckmaster.ca>
2025-01-09 20:38:51 -0800

    Make app work in browser again

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import LatestAirVisualDeviceMeasurement from './LatestAirVisualDeviceMeasurement';
-
 import {  ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { LatestAirVisualDeviceMeasurement } from './LatestAirVisualDeviceMeasurement';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
